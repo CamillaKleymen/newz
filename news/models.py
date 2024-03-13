2fc542910c2a261778f8230d5e56ee762b4596a5
CamillaKleymen <camillakleymen@outlook.com>
2024-03-13 15:20:12 +0500

    edit

---
+++ news/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+
+class CategoryModel(models.Model):
+    category_title = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.category_title
+
+    class Meta:
+        verbose_name = 'News category'
+        verbose_name_plural = 'News categories'
+
+class NewsModel(models.Model):
+    news_title = models.CharField(max_length=50, help_text='here you should enter name of product')
+    news_category = models.ForeignKey(CategoryModel, on_delete=models.CASCADE)
+    news_country = models.CharField(max_length=30)
+    news_count = models.IntegerField()
+    news_description = models.TextField()
+    news_image = models.FileField(upload_to='news_images')
+    news_created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.news_title
+
+    class Meta:
+        verbose_name = 'Announcement'
+        verbose_name_plural = 'News'
+
+
